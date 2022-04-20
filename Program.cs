8b13dc6b0e2a511fd307c1987f0450b503460224
Сергей <seregabur@live.ru>
2022-04-20 11:31:51 +0300

    2

--- Program.cs
+++ Program.cs
@@ -3,25 +3,17 @@ using System.Collections.Generic;
 
 namespace Collection3
 {
-//    В массивах вы выполняли задание "Динамический массив"
-//Используя всё изученное, напишите улучшенную версию динамического массива(не обязательно брать своё старое решение)
-//Задание нужно, чтобы вы освоились с List и прощупали его преимущество.
-//Проверка на ввод числа обязательна.
-
-//Пользователь вводит числа, и программа их запоминает.
-//Как только пользователь введёт команду sum, программа выведет сумму всех веденных чисел.
-
-//Выход из программы должен происходить только в том случае, если пользователь введет команду exit.
     class Program
     {
         static void Main(string[] args)
         {
             Console.Write("Введите любое число и я его запомню\nвведите sum - сложение введеных чисел\nвведите exit - закрытие программы\n");
 
-            bool continueCycle = true;
-            List<int> temp = new List<int>();
+            bool isContinueCycle = true;
 
-            while (continueCycle)
+            List<int> listOfNumbers = new List<int>();
+
+            while (isContinueCycle)
             {
                 string inputString = Console.ReadLine();
 
@@ -31,26 +23,26 @@ namespace Collection3
 
                         int sum = 0;
 
-                        foreach (int number in temp)
+                        foreach (int number in listOfNumbers)
                         {
                             sum += number;
                         }
 
                         Console.WriteLine("Сумма набранных чисел = " + sum);
 
-                        temp.Clear();
+                        listOfNumbers.Clear();
 
                         break;
                     case "exit":
 
-                        continueCycle = false;
+                        isContinueCycle = false;
 
                         break;
                     default:
 
                         if(inputString!="")
                         {
-                            temp.Add(Convert.ToInt32(inputString));
+                            listOfNumbers.Add(Convert.ToInt32(inputString));
                         }
 
                         break;
